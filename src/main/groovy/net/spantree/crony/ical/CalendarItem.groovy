2b497df29ed2613f873526355504d622df5409b4
Gary Turovsky <gary@spantree.net>
2012-11-26 23:52:53 +0000

    Removing time zone hack

--- src/main/groovy/net/spantree/crony/ical/CalendarItem.groovy
+++ src/main/groovy/net/spantree/crony/ical/CalendarItem.groovy
@@ -17,24 +17,9 @@ class CalendarItem {
 	Calendar iCalCalendar
 	static CalendarBuilder builder = new CalendarBuilder();
 	
-	public CalendarItem(String iCalString, DateTimeZone untilTz = DateTimeZone.UTC) {
+	public CalendarItem(String iCalString) {
 		CompatibilityHints.setHintEnabled(CompatibilityHints.KEY_RELAXED_PARSING, true);
 		iCalCalendar = builder.build(new StringReader(iCalString));
-		
-		//Adjust the UNTIL portion of RRULES
-		//iCal4j assumes this is in UTC
-		if(untilTz && untilTz!=DateTimeZone.UTC) {
-			iCalCalendar.getComponents("VEVENT").each{ VEvent vEvent ->
-				def rrule = vEvent.getProperty("RRULE")
-				net.fortuna.ical4j.model.Date until = rrule?.getRecur()?.getUntil()
-				
-				if(until) {
-					int offset = untilTz.getOffset(until.getTime())
-					until.setMinutes(until.getMinutes() - (int)(offset/60000))
-					rrule.getRecur().setUntil(until)
-				}
-			}
-		}
 	}
 	
 	public List<EventItem> getEvents() {
